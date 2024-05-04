0e4603f0673f372193dcece4b1ba0be9def57185
ritali <ritsg11@gmail.com>
2024-05-04 22:09:47 +0530

    bugfix in visitor javascript

--- html/visitor_count.js
+++ html/visitor_count.js
@@ -7,58 +7,54 @@ const serverfetchLoc = 'https://ritali.pythonanywhere.com/api/geo-location'
 // const serverfetchLoc = 'http://localhost:5000/api/geo-location'
 
 // Initialize the map
-var map = L.map('map').setView([-0, 23], 0.2); /* latitude and longitude adjustment for center of the map */
+var map = L.map('map').setView([15, 15], 0.2); /* latitude and longitude adjustment for center of the map */
 
 // Add a tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+            { attribution: '© OpenStreetMap contributors'
+            }).addTo(map);
 
 // Function to load markers on the map
 function loadMarkers(coord) {
     L.marker(coord).addTo(map);
 }
 
-// Function to fetch initial coordinates from the server and then add the new coordinates to map
-function fetchCoordinates_drawPinOnMap() {
-    // fetch the visitor IP
-    fetchIP() 
-    .then(() => {  // Ensuring IP is fetched before fetching geolocation
-        fetch(serverAddress)
-        .then(response => response.json())
-        .then(data => {
-            for (var i = 0; i < data.length; i++) {
-                visitorCoordinates.push(data[i]);
-                loadMarkers(visitorCoordinates[i]); // place pins on map
-            }
-            document.getElementById('visitorCount').textContent = visitorCoordinates.length;
-            fetchGeoLocation();  // Get new geolocation and update server after initial fetch
-        })
-        .catch(error => {
-            console.error('Error fetching initial coordinates:', error);
-        }); 
-    });
-}
-
-// Function to get IP address of visitor
-function fetchIP() {
+// Function to 
+//      -- fetch coordinates from the server using visitor IP
+//      -- put present and past visitor coordinates on map
+//      -- display total unique visitors till present
+function fetchMapCoordinates_putPins() {
     fetch('https://api.ipify.org?format=json')
         .then(response => response.json())
-        .then(data => {
+        .then(data => { 
+            // Ensuring IP is fetched before fetching geolocation  
             visitor_IP = data.ip;
+            fetch(serverAddress)
+            .then(response => response.json())
+            .then(data => {
+                for (var i = 0; i < data.length; i++) {
+                    visitorCoordinates.push(data[i]);
+                    loadMarkers(visitorCoordinates[i]);
+                }
+                document.getElementById('visitorCount').textContent = visitorCoordinates.length;
+                fetchGeoLocation();  // Get new geolocation and update server after initial fetch
+            })
+            .catch(error => {
+                console.error('Error fetching initial coordinates:', error);
+            });
         })
         .catch(error => {
-            console.error('Error fetching the API:', error);
-    });
+            console.error('Error fetching the IP query API:', error);
+        });
 }
 
 // Function to get geolocation and send to the server
 function fetchGeoLocation() {
-fetch(serverfetchLoc, {
-                        method: 'POST',
-                        headers: { 'Content-Type': 'application/json' },
-                        body: JSON.stringify({ ip: visitor_IP })
-    })
+    fetch(serverfetchLoc, {
+                           method: 'POST',
+                           headers: { 'Content-Type': 'application/json' },
+                           body: JSON.stringify({ ip: visitor_IP })
+                           })
     .then(response => response.json())
     .then(data => {
             if(data.status === 'success') {
@@ -76,14 +72,14 @@ fetch(serverfetchLoc, {
 // Function to send coordinates to the server
 function sendCoordinatesToServer(newCoord, org, city) {
     fetch(serverAddress, {
-                        method: 'POST',
-                        headers: { 'Content-Type': 'application/json' },
-                        body: JSON.stringify({ coordinates: newCoord,
-                                                ip: visitor_IP,
-                                                org: org,
-                                                city: city
-                                            })
-    })
+                          method: 'POST',
+                          headers: { 'Content-Type': 'application/json' },
+                          body: JSON.stringify({ coordinates: newCoord,
+                                                 ip: visitor_IP,
+                                                 org: org,
+                                                 city: city
+                                               })
+                         })
     .then(response => response.json())
     .then(data => {
         console.log('Success:', data);
@@ -91,17 +87,17 @@ function sendCoordinatesToServer(newCoord, org, city) {
         fetch(serverAddress)
         .then(response => response.json())
         .then(data => {
-                    for (var i = 0; i < data.length; i++) {
-                        if (i>visitorCoordinates.length-1) {
-                            visitorCoordinates.push(data[i]); // add new position
-                        }
-                        loadMarkers(visitorCoordinates[i]); // again add all pins on map
-                    }
-                    document.getElementById('visitorCount').textContent = visitorCoordinates.length;
-            })
-            .catch(error => {
-                console.error('Error re-fetching updated coordinates:', error);
-            });
+            for (var i = 0; i < data.length; i++) {
+                if (i>visitorCoordinates.length-1) {
+                    visitorCoordinates.push(data[i]); // add new position
+                }
+                loadMarkers(visitorCoordinates[i]); // again add all pins on map
+            }
+            document.getElementById('visitorCount').textContent = visitorCoordinates.length;
+        })
+        .catch(error => {
+            console.error('Error re-fetching updated coordinates:', error);
+        });
     })
     .catch(error => {
         console.error('Error sending coordinates:', error);
@@ -110,5 +106,5 @@ function sendCoordinatesToServer(newCoord, org, city) {
 
 // On page load, fetch initial coordinates
 window.onload = function() {
-    fetchCoordinates_drawPinOnMap();
-};
+    fetchMapCoordinates_putPins();
+}
